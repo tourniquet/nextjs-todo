5c6e0a5baf4f2be9c48ed2acd37a7b6a1f9c68e8
tourniquet <admyn3d@gmail.com>
2023-05-20 20:45:28 +0100

    minor changes

--- components/NewItem/NewItem.tsx
+++ components/NewItem/NewItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const Input = styled.input.attrs({
@@ -15,8 +15,8 @@ const Input = styled.input.attrs({
 
 let nextId = 0
 
-export default function NewItem () {
-  const [todo, setTodo] = useState('') // FIXME: replace todo with something else?
+export default function NewItem (): JSX.Element {
+  const [todo, setTodo] = useState('')
   const [todos, setTodos] = useState<any[]>([])
 
   return (
@@ -27,21 +27,21 @@ export default function NewItem () {
         value={todo}
         onChange={e => setTodo(e.target.value)}
         onKeyDown={e => {
-          if (e.code === 'Enter' && todo) {
-          setTodos([
-            ...todos,
-            { id: nextId++, name: todo }
-          ])
-          setTodo('')
+          if (e.code === 'Enter' && Boolean(todo)) {
+            setTodos([
+              ...todos,
+              { id: nextId++, todo }
+            ])
+            setTodo('')
           }
         }}
       />
 
       <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>{todo.name}</li>
+        {todos.map(item => (
+          <li key={item.id}>{item.todo}</li>
         ))}
       </ul>
-      </>
+    </>
   )
 }
